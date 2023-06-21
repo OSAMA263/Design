84f7ca87741e54c12b56cad76e1bef001b0bdd9b
Osam k <74204122+OSAMA263@users.noreply.github.com>
2023-06-21 14:10:38 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,7 @@ import "./index.css";
 import { BrowserRouter, HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
     <HashRouter>
       <App />
-    </HashRouter>
   </React.StrictMode>
 );
